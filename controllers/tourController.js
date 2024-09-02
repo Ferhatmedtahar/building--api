e7e71b097d487e514652e0e385d8f176da24131b
ferhat <ferhattahar46@gmail.com>
2024-09-02 18:33:01 +0100

    create the utils folder and create file for the class for refactored code

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/TourModel');
+const APIFeatures = require('./utils/apiFeatures');
 
 // exports.checkBody = (req, res, next) => {
 //   if (!req.body?.name || !req.body?.price) {
@@ -19,59 +20,6 @@ exports.aliasTopTours = (req, res, next) => {
   };
   next();
 };
-
-class APIFeatures {
-  constructor(query, queryString) {
-    this.query = query;
-    this.queryString = queryString;
-  }
-  //1 filter
-
-  filter() {
-    const queryObj = { ...this.queryString };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach((el) => delete queryObj[el]);
-    //the values which we want to specify : gte , gt, lte , lt
-    const queryStr = JSON.stringify(queryObj).replace(
-      /\b(gte|gt|lte|lt)\b/g,
-      (match) => `$${match}`,
-    );
-    this.query.find(JSON.parse(queryStr));
-    // let this.query = Tour.find(JSON.parse(queryStr));
-    return this;
-  }
-
-  //2sort
-  sort() {
-    if (this.queryString.sort) {
-      const sortBy = this.queryString.sort.split(',').join(' ');
-      this.query = this.query.sort(sortBy);
-    } else {
-      this.query = this.query.sort('-createdAt');
-    }
-    return this;
-  }
-  //3fields :project
-
-  fields() {
-    if (this.queryString.fields) {
-      const fields = this.queryString.fields.split(',').join(' ');
-      this.query.select(fields);
-    } else {
-      this.query.select('-__v');
-    }
-    return this;
-  }
-  //4pagination
-
-  paginate() {
-    const page = this.queryString.page * 1 || 1;
-    const limit = this.queryString.limit * 1 || 10;
-    const skip = (page - 1) * limit; //data before the start of the page
-    this.query = this.query.skip(skip).limit(limit);
-    return this;
-  }
-}
 
 exports.getAllTours = async (req, res) => {
   try {
